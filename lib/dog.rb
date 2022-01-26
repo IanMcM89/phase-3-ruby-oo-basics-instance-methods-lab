9713785ed743a866ca0fbe0bfed9a2de3b3bad28
Ian <ianmcm.89@gmail.com>
2022-01-26 10:43:23 -0500

    commit tree 91b6e2c7efa58e8f20d6bbfca7cd23d62396c96b

---
+++ lib/dog.rb
@@ -0,0 +1,31 @@
+class Dog
+    #Class body
+
+    #Instance method definitions
+
+    #bark
+    def bark
+        puts "Woof!"
+    end
+
+    #sit
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+class Person
+    #Class body
+
+    #Instance method definitions
+
+    #talk
+    def talk
+        puts "Hello World!"
+    end
+
+    #walk
+    def walk
+        puts "The Person is walking"
+    end
+end
